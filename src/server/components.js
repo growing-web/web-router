cac651c062e94724249c8e6fe1585a7bc74e4132
糖饼 <sugarpie.tang@gmail.com>
2022-08-01 18:55:19 +0800

    refactor: 清理无用代码

--- src/server/components.js
+++ src/server/components.js
@@ -70,24 +70,24 @@ export function Routemap(routemap) {
   </script>`;
 }
 
-export function ScrollRestoration() {
-  let STORAGE_KEY = "positions";
-  let restoreScroll = ((STORAGE_KEY) => {
-    if (!window.history.state || !window.history.state.key) {
-      let key = Math.random().toString(32).slice(2);
-      window.history.replaceState({ key }, "");
-    }
-    try {
-      let positions = JSON.parse(sessionStorage.getItem(STORAGE_KEY) || "{}");
-      let storedY = positions[window.history.state.key];
-      if (typeof storedY === "number") {
-        window.scrollTo(0, storedY);
-      }
-    } catch (error) {
-      console.error(error);
-      sessionStorage.removeItem(STORAGE_KEY);
-    }
-  }).toString();
+// export function ScrollRestoration() {
+//   let STORAGE_KEY = "positions";
+//   let restoreScroll = ((STORAGE_KEY) => {
+//     if (!window.history.state || !window.history.state.key) {
+//       let key = Math.random().toString(32).slice(2);
+//       window.history.replaceState({ key }, "");
+//     }
+//     try {
+//       let positions = JSON.parse(sessionStorage.getItem(STORAGE_KEY) || "{}");
+//       let storedY = positions[window.history.state.key];
+//       if (typeof storedY === "number") {
+//         window.scrollTo(0, storedY);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       sessionStorage.removeItem(STORAGE_KEY);
+//     }
+//   }).toString();
 
-  return html`<script>${unsafeHTML(`(${restoreScroll})(${JSON.stringify(STORAGE_KEY)})`)}</script>`
-}
+//   return html`<script>${unsafeHTML(`(${restoreScroll})(${JSON.stringify(STORAGE_KEY)})`)}</script>`
+// }
